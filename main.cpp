438527fec17e52249169742adfa379e78af592ef
logan-a-richey <loganricheygithub@gmail.com>
2025-02-07 00:37:01 -0600

    updated cpp main

--- main.cpp
+++ main.cpp
@@ -5,14 +5,6 @@ Author: Logan Richey
 Date:   Feb 4, 2025
 ******************************************************************************/
 
-/*
-Compile and run demo program using:
-The demo midi output files should appear in an output directory.
-
-g++ -std=c++11 -o main midi_writer.cpp main.cpp
-./main
-*/
-
 #include "tests.h"
 
 int main(){
